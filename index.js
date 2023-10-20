e734064206e018a1a3ce68ed821995c6b07af403
jimmymakumi <146829497+jimmymakumi@users.noreply.github.com>
2023-10-21 00:38:47 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31, num2 = 2;
+let multiply = num1 * num2;
+let num3 = 4, num4 = 6
+let random = num4 - num3
+let num5 = 54, num6 = 10
+let mod = num5 % num6
+let num7 = 5, num8 = 8, num9 = 15, num10 = 20
+let max = (5,8,15,20)
